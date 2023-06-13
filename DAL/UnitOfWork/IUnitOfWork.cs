3d63b08c0dfb3cd5a6f11f150e3202a67736a349
Yulia K <resomniosae@gmail.com>
2023-06-13 14:17:56 +0300

    Add repository wrapper

---
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Repository_Interfaces;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IGuideRepository Guides { get; }
+        IGuideTourRepository GuideTours { get; }
+        IReviewRepository Reviews { get; }
+        IRoleRepository Roles { get; }
+        ISightRepository Sights { get; }
+        ITicketRepository Tickets { get; }
+        ITourRepository Tours { get; }
+        ITourSightRepository TourSights { get; }
+        IUserRepository Users { get; }
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Repository_Interfaces;
+
+namespace DAL.UnitOfWork
+{
+    internal class UnitOfWork:IUnitOfWork, IDisposable
+    {
+        private readonly SightseeingdbContext _context;
+
+        private IGuideRepository _guideRepository;
+        private IGuideTourRepository _guideTourRepository;
+        private IReviewRepository _reviewRepository;
+        private IRoleRepository _roleRepository;
+        private ISightRepository _sightRepository;
+        private ITicketRepository _ticketRepository;
+        private ITourRepository _tourRepository;
+        private ITourSightRepository _tourSightRepository;
+        private IUserRepository _userRepository;
+        private bool _disposed;
+
+        public UnitOfWork(SightseeingdbContext context)
+        {
+            _context = context;
+        }
+
+        public IGuideRepository Guides { get { return _guideRepository; } }
+        public IGuideTourRepository GuideTours { get { return _guideTourRepository;} }
+        public IReviewRepository Reviews { get { return _reviewRepository; } }
+        public IRoleRepository Roles { get { return _roleRepository;} }
+        public ISightRepository Sights { get { return _sightRepository;} }
+        public ITicketRepository Tickets { get { return _ticketRepository;} }
+        public ITourRepository Tours { get { return _tourRepository;} }
+        public ITourSightRepository TourSights { get { return _tourSightRepository;} }
+        public IUserRepository Users { get { return _userRepository;} }
+
+        public async Task SaveChangesAsync()
+        {
+                await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
