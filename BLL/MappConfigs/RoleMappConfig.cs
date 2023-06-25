5f2f6366290d077188f423e3a03733b78b97d6e7
Yulia K <resomniosae@gmail.com>
2023-06-26 01:32:57 +0300

    remove unused using-statements

--- BLL/MappConfigs/RoleMappConfig.cs
+++ BLL/MappConfigs/RoleMappConfig.cs
@@ -1,12 +1,6 @@
 ﻿using AutoMapper;
 using BLL.Models;
 using DAL.Entities;
-using DocumentFormat.OpenXml.Bibliography;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL.MappConfigs
 {
